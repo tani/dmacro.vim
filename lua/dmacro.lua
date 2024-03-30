20ba4021fa68a38cebb7d1256a4bf0f9364a1af5
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-03-30 12:45:19 +0000

    Format code

--- lua/dmacro.lua
+++ lua/dmacro.lua
@@ -4,69 +4,69 @@ local function guess_completion_1()
 		local span = vim.list_slice(hist, 1, i)
 		local spanspan = vim.fn.extend(span, span)
 		local double = vim.list_slice(hist, 1, i * 2)
-        if vim.deep_equal(double, spanspan) then
-            return span
-        end
+		if vim.deep_equal(double, spanspan) then
+			return span
+		end
 	end
-    return nil
+	return nil
 end
 
 local function guess_completion_2()
 	local hist = vim.fn.reverse(vim.b.dmacro_history)
-    for i = math.floor(#hist / 2), 1, -1 do
-        local span = vim.list_slice(hist, 1, i)
-        for j = #span, #hist - #span do
-            local prevspan = vim.list_slice(hist, j + 1, j + #span)
-            if vim.deep_equal(prevspan, span) then
-                return vim.list_slice(hist, #span + 1, j)
-            end
-        end
-    end
-    return nil
+	for i = math.floor(#hist / 2), 1, -1 do
+		local span = vim.list_slice(hist, 1, i)
+		for j = #span, #hist - #span do
+			local prevspan = vim.list_slice(hist, j + 1, j + #span)
+			if vim.deep_equal(prevspan, span) then
+				return vim.list_slice(hist, #span + 1, j)
+			end
+		end
+	end
+	return nil
 end
 
 local function setup(opts)
-    opts = opts or {}
-    local dmacro_key = opts.dmacro_key or "<leader>."
-    local this_group = vim.api.nvim_create_augroup("dmacro", {})
-    vim.api.nvim_create_autocmd("BufWinEnter", {
-        group = this_group,
-        callback = function()
-            vim.b.dmacro_history = {}
-            vim.b.prev_completion = nil
-        end,
-    })
-    vim.on_key(function(key, typed)
+	opts = opts or {}
+	local dmacro_key = opts.dmacro_key or "<leader>."
+	local this_group = vim.api.nvim_create_augroup("dmacro", {})
+	vim.api.nvim_create_autocmd("BufWinEnter", {
+		group = this_group,
+		callback = function()
+			vim.b.dmacro_history = {}
+			vim.b.prev_completion = nil
+		end,
+	})
+	vim.on_key(function(key, typed)
 		if typed ~= "" and typed ~= nil then
 			vim.b.dmacro_history = vim.fn.add(vim.b.dmacro_history, typed)
-            if typed ~= dmacro_key:gsub("<leader>", [[\]]) then
-                if vim.b.prev_completion then
-                    vim.b.prev_completion = nil
-                    vim.b.dmacro_history = {}
-                end
-            end
+			if typed ~= dmacro_key:gsub("<leader>", [[\]]) then
+				if vim.b.prev_completion then
+					vim.b.prev_completion = nil
+					vim.b.dmacro_history = {}
+				end
+			end
 		end
-    end)
+	end)
 	vim.keymap.set({ "i" }, dmacro_key, function()
-        vim.b.dmacro_history = vim.list_slice(vim.b.dmacro_history, 1, #vim.b.dmacro_history - 1)
-        local completion = vim.b.prev_completion
-        completion = completion or guess_completion_1()
-        if completion then
-            print("play: " .. table.concat(vim.fn.reverse(completion)))
-            vim.fn.feedkeys(table.concat(vim.fn.reverse(completion)))
-            vim.b.dmacro_history = vim.fn.extend(vim.b.dmacro_history, vim.fn.reverse(completion))
-            vim.b.prev_completion = completion
-            return
-        end
-        completion = completion or guess_completion_2()
-        if completion then
-            print("play: " .. table.concat(vim.fn.reverse(completion)))
-            vim.fn.feedkeys(table.concat(vim.fn.reverse(completion)))
-            vim.b.dmacro_history = vim.fn.extend(vim.b.dmacro_history, vim.fn.reverse(completion))
-            vim.b.prev_completion = nil
-            return
-        end
-    end)
+		vim.b.dmacro_history = vim.list_slice(vim.b.dmacro_history, 1, #vim.b.dmacro_history - 1)
+		local completion = vim.b.prev_completion
+		completion = completion or guess_completion_1()
+		if completion then
+			print("play: " .. table.concat(vim.fn.reverse(completion)))
+			vim.fn.feedkeys(table.concat(vim.fn.reverse(completion)))
+			vim.b.dmacro_history = vim.fn.extend(vim.b.dmacro_history, vim.fn.reverse(completion))
+			vim.b.prev_completion = completion
+			return
+		end
+		completion = completion or guess_completion_2()
+		if completion then
+			print("play: " .. table.concat(vim.fn.reverse(completion)))
+			vim.fn.feedkeys(table.concat(vim.fn.reverse(completion)))
+			vim.b.dmacro_history = vim.fn.extend(vim.b.dmacro_history, vim.fn.reverse(completion))
+			vim.b.prev_completion = nil
+			return
+		end
+	end)
 end
 
 return { setup = setup }
